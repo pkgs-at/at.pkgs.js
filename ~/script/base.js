17eea5533c2ee63e8ab2338c8dc803334b2d1994
Sotaro SUZUKI <sotaro.suzuki@architector.jp>
2014-09-02 17:56:36 +0900

    サイト用リソース追加

---
+++ ~/script/base.js
@@ -0,0 +1,8 @@
+(function(root, unasigned) {
+	$.at.toText.datetimeFormat = '{yyyy}/{MM}/{dd} {HH}:{mm}';
+	$.at.toText.dateFormat = '{yyyy}/{MM}/{dd}';
+	$.at.toText.timeFormat = '{HH}:{mm}';
+	$.at.toJSON.datetimeFormat = '{yyyy}-{MM}-{dd} {HH}:{mm}:{ss}';
+	$.at.toJSON.dateFormat = '{yyyy}-{MM}-{dd}';
+	$.at.toJSON.timeFormat = '{HH}:{mm}:{ss}';
+})(this);
